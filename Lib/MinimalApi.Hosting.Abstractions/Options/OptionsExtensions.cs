0c0314475d035d928c2b23620bc2383d7c9397a1
bog1978 <bog1978@mail.ru>
2026-08-19 10:29:26 +0300

    Перевел на .Net 10

--- Lib/MinimalApi.Hosting.Abstractions/Options/OptionsExtensions.cs
+++ Lib/MinimalApi.Hosting.Abstractions/Options/OptionsExtensions.cs
@@ -8,33 +8,49 @@ namespace MinimalApi.Hosting.Options;
 
 public static class OptionsExtensions
 {
-    public static IServiceCollection BindOptions<TOptions>(this IServiceCollection services, IConfigurationRoot configuration)
-        where TOptions : class, IOptionsBase
+    public static IServiceCollection BindOptions<TOptions>(
+        this IServiceCollection services,
+        IConfigurationRoot configuration)
+        where TOptions : class, IOptionsBase =>
+        BindOptions<TOptions>(services, configuration, TOptions.SectionName);
+
+    public static IServiceCollection BindOptions<TOptions>(
+        this IServiceCollection services,
+        IConfigurationRoot configuration,
+        string sectionName)
+        where TOptions : class
     {
-        var section = configuration.GetSection(TOptions.SectionName);
+        var section = configuration.GetSection(sectionName);
         services
             .AddOptions<TOptions>()
             .Bind(section)
             .ValidateDataAnnotations()
             .Validate(
                 _ => section.Exists(),
-                $"Раздел конфигурации [{TOptions.SectionName}] отсутствует.")
+                $"Раздел конфигурации [{sectionName}] отсутствует.")
             .ValidateOnStart();
         return services;
     }
 
-    public static TOptions GetOptions<TOptions>(this IConfiguration configuration)
-        where TOptions : class, IOptionsBase
+    public static TOptions GetOptions<TOptions>(
+        this IConfiguration configuration)
+        where TOptions : class, IOptionsBase =>
+        configuration.GetOptions<TOptions>(TOptions.SectionName);
+
+    public static TOptions GetOptions<TOptions>(
+        this IConfiguration configuration,
+        string sectionName)
+        where TOptions : class
     {
-        var section = configuration.GetSection(TOptions.SectionName);
+        var section = configuration.GetSection(sectionName);
         if (!section.Exists())
             throw new InvalidOperationException(
-                $"Раздел конфигурации [{TOptions.SectionName}] отсутствует.");
+                $"Раздел конфигурации [{sectionName}] отсутствует.");
 
         var options =
             section.Get<TOptions>()
             ?? throw new InvalidOperationException(
-                $"Не удалось привязать секцию [{TOptions.SectionName}] " +
+                $"Не удалось привязать секцию [{sectionName}] " +
                 $"к классу настроек [{typeof(TOptions).FullName}].");
 
         // Валидация через DataAnnotations
--- Storage.Api/Program.cs
+++ Storage.Api/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using MinimalApi.Hosting;
 using MinimalApi.Hosting.Options;
 using Npgsql;
