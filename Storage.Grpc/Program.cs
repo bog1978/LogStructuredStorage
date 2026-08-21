a4522fbd2b460bc4253eb4b7a828f87321538ca8
bog1978 <bog1978@mail.ru>
2026-08-21 10:56:20 +0300

    Добавил заготовку для gRPC сервиса.

---
+++ Storage.Grpc/Program.cs
@@ -0,0 +1,28 @@
+using Storage.Grpc.Services;
+
+namespace Storage.Grpc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services
+                .AddGrpcReflection()
+                .AddGrpc();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseRouting();
+            app.MapGrpcService<GreeterService>();
+            app.MapGrpcReflectionService();
+            
+            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+            app.Run();
+        }
+    }
+}
